f1a6306b3b9f934a84a13530e378ba0dd2cfebb6
ramblingsofadev <dbyoung2@gmail.com>
2014-10-15 14:15:20 -0500

    Added a credential type Id to the credential repo interface

--- application/rdev/models/authentication/credentials/orm/credential/IRepo.php
+++ application/rdev/models/authentication/credentials/orm/credential/IRepo.php
@@ -11,11 +11,12 @@ use RDev\Models\ORM\Repositories;
 interface IRepo extends Repositories\IRepo
 {
     /**
-     * Gets all the active credentials that belong to an entity
+     * Gets all the active credentials of the input type that belong to an entity
      *
      * @param int|string $entityId The Id of the entity
      * @param int $entityTypeId The type of entity
+     * @param int $credentialTypeId The type of the credentials to return
      * @return Credentials\Credential[] The list of all credentials owned by the entity
      */
-    public function getAllActiveByEntityId($entityId, $entityTypeId);
+    public function getAllActiveByEntityId($entityId, $entityTypeId, $credentialTypeId);
 } 
