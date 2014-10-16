fed099cf6f51c48d7a31a866c1ec54977c04de74
ramblingsofadev <dbyoung2@gmail.com>
2014-10-16 11:09:47 -0500

    Directly call date_default_timezone_set("UTC") in the PHPUnit config because Travis is bombing out

--- configs/phpunit.php
+++ configs/phpunit.php
@@ -5,3 +5,6 @@
  * Sets up PHPUnit tests
  */
 require_once(__DIR__ . "/../vendor/autoload.php");
+
+// Set the default timezone in case the test server doesn't have it already set
+date_default_timezone_set("UTC");
