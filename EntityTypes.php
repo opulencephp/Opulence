6322936f7550dc0d9e860c9d054d7311d3933888
ramblingsofadev <dbyoung2@gmail.com>
2014-11-04 13:59:37 -0600

    Squashed 'app/rdev/authentication/' content from commit 0727195
    git-subtree-dir: app/rdev/authentication
    git-subtree-split: 07271957b7fc82cd008a1972001549cd1908a6d8

---
+++ EntityTypes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (C) 2014 David Young
+ *
+ * Defines different types of entities that can be authenticated
+ */
+namespace RDev\Authentication;
+
+class EntityTypes
+{
+    /** A user */
+    const USER = 1;
+    /** An application */
+    const APPLICATION = 2;
+} 
