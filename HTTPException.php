34b9d446ba9bb9c7944e643a713df600cf59db82
ramblingsofadev <dbyoung2@gmail.com>
2014-11-03 19:57:03 -0600

    Squashed 'application/rdev/http/' content from commit 4767f09
    git-subtree-dir: application/rdev/http
    git-subtree-split: 4767f09b028d7b08385417e66eb48ebf434d72e5

---
+++ HTTPException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (C) 2014 David Young
+ *
+ * Defines an exception that is thrown by an HTTP component
+ */
+namespace RDev\HTTP;
+use RDev\Exceptions;
+
+class HTTPException extends Exceptions\Exception
+{
+    // Don't do anything
+} 
