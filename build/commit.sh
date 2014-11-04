0ae4ac24fc4e68c5a39b38a98be8a72b8fe92908
ramblingsofadev <dbyoung2@gmail.com>
2014-11-04 09:00:00 -0600

    Messing around with subtree stuff

--- build/commit.sh
+++ build/commit.sh
@@ -1,3 +1,1 @@
-if git diff applications/master master:application/rdev/applications; then
-    git subtree push --prefix=application/rdev/applications --squash applications master
-fi
+git subtree push --prefix=application/rdev/applications --squash applications master
