c265c3b410d1165b885121d5a6c37d04f2cccfbe
ramblingsofadev <dbyoung2@gmail.com>
2014-11-03 19:23:28 -0600

    Squashed 'application/rdev/authentication/' content from commit b6a1d4d
    git-subtree-dir: application/rdev/authentication
    git-subtree-split: b6a1d4d70495f4000f2282ac5fd1c631b8419f49

---
+++ credentials/CredentialTypes.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (C) 2014 David Young
+ *
+ * Defines the different types of credentials
+ */
+namespace RDev\Authentication\Credentials;
+
+class CredentialTypes
+{
+    /** A password */
+    const PASSWORD = 1;
+    /** A login credential */
+    const LOGIN = 2;
+    /** A credential used to authenticate the identity of an entity, but not log it in */
+    const AUTHENTICATION = 3;
+} 
