2202e5b51780343c08ef5d9526ac0246516fa227
ramblingsofadev <dbyoung2@gmail.com>
2015-03-10 12:21:22 -0500

    Set PHPUnit to display errors to track down PHP7 bug

--- configs/phpunit.php
+++ configs/phpunit.php
@@ -6,5 +6,10 @@
  */
 require_once __DIR__ . "/../vendor/autoload.php";
 
+// Turn on error reporting
+ini_set("error_reporting", E_ALL);
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
+
 // Set the default timezone in case the test server doesn't have it already set
 date_default_timezone_set("UTC");
