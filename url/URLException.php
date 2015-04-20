7cddf0546d5b7eef905412c08f58b06df3059b64
ramblingsofadev <dbyoung2@gmail.com>
2015-04-20 18:06:12 -0500

    Moved RDev\HTTP\Routing back to RDev\Routing, moved kernels into RDev\Framework

---
+++ url/URLException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (C) 2015 David Young
+ *
+ * Defines a URL exception
+ */
+namespace RDev\Routing\URL;
+use Exception;
+
+class URLException extends Exception
+{
+    // Don't do anything
+} 
