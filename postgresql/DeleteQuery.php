3274c79dfa43ede2bfce74fc9f239d503f129d82
ramblingsofadev <dbyoung2@gmail.com>
2015-04-28 11:28:32 -0500

    Split Memcached, Redis, QueryBuilders, and Forms into their own libraries, renamed RDev\Databases\SQL namespace to RDev\Databases, updated changelog

---
+++ postgresql/DeleteQuery.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (C) 2015 David Young
+ *
+ * Builds a delete query
+ */
+namespace RDev\QueryBuilders\PostgreSQL;
+use RDev\QueryBuilders\DeleteQuery as BaseDeleteQuery;
+
+class DeleteQuery extends BaseDeleteQuery
+{
+    // Don't do anything
+} 
