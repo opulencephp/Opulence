2073408f23ae5f0d4bd5deecb743facc12623cb8
opulencephp <dbyoung2@gmail.com>
2015-10-08 21:03:48 -0500

    Incrementing version

--- app/opulence/applications/Application.php
+++ app/opulence/applications/Application.php
@@ -17,7 +17,7 @@ use RuntimeException;
 class Application
 {
     /** @var string The current Opulence version */
-    private static $version = "0.6.12";
+    private static $version = "0.6.13";
     /** @var Paths The paths to various directories used by Opulence */
     private $paths = null;
     /** @var IDispatcher The task dispatcher */
