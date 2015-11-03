3e23cb909b978d1fc1e86fcad3f857966afc30e3
opulencephp <dbyoung2@gmail.com>
2015-11-03 08:26:31 -0600

    Manually re-added all files

---
+++ Tasks/Dispatchers/IDispatcher.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2015 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Applications\Tasks\Dispatchers;
+
+/**
+ * Defines the interface for task dispatchers to implement
+ */
+interface IDispatcher
+{
+    /**
+     * Dispatches all events of a particular type
+     *
+     * @param string $taskType The type of tasks to dispatch
+     */
+    public function dispatch($taskType);
+
+    /**
+     * Registers a task of a certain type
+     *
+     * @param string $taskType The type of task being registered
+     * @param callable $task The task to run
+     */
+    public function registerTask($taskType, callable $task);
+}
---
+++ Tasks/TaskTypes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2015 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Applications\Tasks;
+
+/**
+ * Defines the different types of tasks
+ */
+class TaskTypes
+{
+    /** Pre-shutdown tasks */
+    const PRE_SHUTDOWN = "preShutdown";
+    /** Pre-start tasks */
+    const PRE_START = "preStart";
+    /** Post-start tasks */
+    const POST_SHUTDOWN = "postShutdown";
+    /** Post-shutdown tasks */
+    const POST_START = "postStart";
+}
