74dbe62ce0ae0831108c04d3ac97453e38bb0614
opulencephp <dbyoung2@gmail.com>
2015-11-03 11:00:25 -0600

    Fixed capitalization

---
+++ OrmException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2015 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Orm;
+
+use Exception;
+
+/**
+ * Defines an Orm exception
+ */
+class OrmException extends Exception
+{
+    // Don't do anything
+}
