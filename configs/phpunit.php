5141042cc482d88ca0d6e0c963dc07ad2a22571d
opulencephp <dbyoung2@gmail.com>
2015-11-06 09:07:44 -0600

    Incremented version number

--- configs/phpunit.php
+++ configs/phpunit.php
@@ -6,9 +6,6 @@
  * @copyright Copyright (C) 2015 David Young
  * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
  */
-/**
- * Sets up PHPUnit tests
- */
 require_once __DIR__ . "/../vendor/autoload.php";
 
 // Set the default timezone in case the test server doesn't have it already set
