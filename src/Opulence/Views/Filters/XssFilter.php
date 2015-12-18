e84766faf335897b4639de4d1fd91abf578ba691
opulencephp <dbyoung2@gmail.com>
2015-12-18 08:30:58 -0600

    Removed unnecessary mb_convert_encoding

--- src/Opulence/Views/Filters/XssFilter.php
+++ src/Opulence/Views/Filters/XssFilter.php
@@ -30,7 +30,6 @@ class XssFilter implements IFilter
             $filteredInput = str_replace("'", "%27", $input);
         }
 
-        $filteredInput = mb_convert_encoding($filteredInput, "UTF-8", "UTF-8");
         $filteredInput = filter_var($filteredInput, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
         return $filteredInput;
