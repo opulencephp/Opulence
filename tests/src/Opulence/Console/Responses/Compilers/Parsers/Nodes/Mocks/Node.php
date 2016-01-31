b9e9d3cc1d9d192eef7132c75adc5c56d4496289
opulencephp <dbyoung2@gmail.com>
2016-01-31 17:44:45 -0600

    Added a file I forgot

---
+++ tests/src/Opulence/Console/Responses/Compilers/Parsers/Nodes/Mocks/Node.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2016 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Tests\Console\Responses\Compilers\Parsers\Nodes\Mocks;
+
+use Opulence\Console\Responses\Compilers\Parsers\Nodes\Node as BaseNode;
+
+/**
+ * Mocks a node for use in testing
+ */
+class Node extends BaseNode
+{
+    /**
+     * @inheritdoc
+     */
+    public function isTag() : bool
+    {
+        return false;
+    }
+}
