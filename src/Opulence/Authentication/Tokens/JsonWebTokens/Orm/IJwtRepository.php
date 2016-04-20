dca25a71f1e6f6988f82d691f1f671bd3dab9233
opulencephp <dbyoung2@gmail.com>
2016-04-19 22:08:57 -0500

    Renamed refresh token repository to JWT repository

---
+++ src/Opulence/Authentication/Tokens/JsonWebTokens/Orm/IJwtRepository.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2016 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Authentication\Tokens\JsonWebTokens\Orm;
+
+use Opulence\Authentication\Tokens\JsonWebTokens\SignedJwt;
+
+/**
+ * Defines the interface for JWT repositories to implement
+ */
+interface IJwtRepository
+{
+    /**
+     * Adds a token for a subject
+     *
+     * @param SignedJwt $jwt The token to add
+     */
+    public function add(SignedJwt &$jwt);
+
+    /**
+     * Deletes a token
+     *
+     * @param SignedJwt $jwt The token to delete
+     */
+    public function delete(SignedJwt $jwt);
+
+    /**
+     * Deletes all tokens for a subject
+     *
+     * @param string $subjectId The Id of the subject whose tokens we're deleting
+     */
+    public function deleteAllForSubject(string $subjectId);
+
+    /**
+     * Gets all the tokens
+     *
+     * @return SignedJwt[] The list of all the tokens
+     */
+    public function getAll() : array;
+
+    /**
+     * Gets the token with the input Id
+     *
+     * @param int|string $id The Id of the token
+     * @return SignedJwt The token
+     */
+    public function getById($id) : SignedJwt;
+
+    /**
+     * Checks if a token exists
+     *
+     * @param SignedJwt $jwt The token to search for
+     * @return bool True if the token exists, otherwise false
+     */
+    public function has(SignedJwt $jwt) : bool;
+}
