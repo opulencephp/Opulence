7a2b7b6f3a8e764d554b9418902b7d09f971ac27
opulencephp <dbyoung2@gmail.com>
2016-06-09 17:48:47 -0500

    Added client classes

---
+++ src/Opulence/Authentication/Clients/Orm/IClientRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2016 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+namespace Opulence\Authentication\Clients\Orm;
+
+use Opulence\Authentication\Clients\IClient;
+
+/**
+ * Defines the interface for client repositories to implement
+ */
+interface IClientRepository
+{
+    /**
+     * Adds a client
+     * 
+     * @param IClient $client The client to add
+     */
+    public function add(IClient &$client);
+
+    /**
+     * Deletes a client
+     * 
+     * @param IClient $client The client to delete
+     */
+    public function delete(IClient $client);
+
+    /**
+     * Gets the client with the input Id
+     * 
+     * @param int|string $id The Id to get by
+     * @return IClient The client with the input Id
+     */
+    public function getById($id);
+}
