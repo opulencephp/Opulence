2b93eafcf62edcf3b851846621e93ffa8f04d673
opulencephp <dbyoung2@gmail.com>
2017-10-10 19:53:14 -0500

    Fixed headers

--- src/Opulence/Collections/Tests/StackTest.php
+++ src/Opulence/Collections/Tests/StackTest.php
@@ -5,7 +5,7 @@
  *
  * @link      https://www.opulencephp.com
  * @copyright Copyright (C) 2017 David Young
- * @license   https://github.com/opulencephp/collections/blob/master/LICENSE.md
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
  */
 
 namespace Opulence\Collections\Tests;
