a0b072a9559c014044334d20a79ee7bde443492f
opulencephp <dbyoung2@gmail.com>
2017-10-21 17:50:06 -0500

    Added collections to build script

--- build/git.sh
+++ build/git.sh
@@ -1,4 +1,4 @@
-REPOS=(Applications Authentication Authorization Cache Console Cryptography Databases Debug Environments Events Framework Http IO Ioc Memcached Orm Pipelines QueryBuilders Redis Routing Sessions Validation Views)
+REPOS=(Applications Authentication Authorization Cache Collections Console Cryptography Databases Debug Environments Events Framework Http IO Ioc Memcached Orm Pipelines QueryBuilders Redis Routing Sessions Validation Views)
 BRANCHES_TO_SPLIT=(master 1.0)
 
 function checkOutPullRequest()
