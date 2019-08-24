f5f86c6343cf08c65653e4eee9fa542d44377258
davidbyoung <dbyoung2@gmail.com>
2019-08-23 20:13:21 -0500

    Simplified build script by scanning for directories rather than hard-coding them

--- bin/split.sh
+++ bin/split.sh
@@ -22,13 +22,15 @@ then
     exit 1
 fi
 
-REPOS=(Authentication Authorization Cache Collections Console Cryptography Databases Debug Environments Events Framework Http IO Ioc Memcached Orm Pipelines QueryBuilders Redis Routing Sessions Validation Views)
+for prefix in src/Opulence/*/ ;
+do
+    repo=$(basename "$prefix")
+    remote=$(echo "$repo" | awk '{print tolower($0)}')
 
-function split()
-{
-    prefix=$1
-    remote=$2
-    echo "Splitting $prefix"
+    echo "Adding remote $remote"
+    git remote add "$remote" https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/opulencephp/$remote.git >/dev/null 2>&1
+
+    echo "Splitting $repo"
     sha=$(./bin/splitsh-lite --prefix="$prefix")
 
     if [ -z "$sha" ]
@@ -38,13 +40,6 @@ function split()
     fi
 
     # Push to the subtree's repo, and do not leak any sensitive info in the logs
-    echo "Pushing $prefix to $remote"
+    echo "Pushing $repo to $remote"
     git push "$remote" "$sha:refs/heads/$GIT_BRANCH" -f >/dev/null 2>&1
-}
-
-for repo in ${REPOS[@]}
-do
-    lower_repo=$(echo "$repo" | awk '{print tolower($0)}')
-    git remote add "$lower_repo" https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/opulencephp/$lower_repo.git >/dev/null 2>&1
-    split "src/Opulence/$repo" "$lower_repo"
 done
