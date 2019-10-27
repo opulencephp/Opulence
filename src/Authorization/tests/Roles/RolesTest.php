612ca2023df11a5af7c389738f14f0ea361a9ca1
davidbyoung <dbyoung2@gmail.com>
2019-10-27 10:55:15 -0500

    Fixed bug that caused role memberships rather than roles to be returned

--- src/Authorization/tests/Roles/RolesTest.php
+++ src/Authorization/tests/Roles/RolesTest.php
@@ -235,6 +235,6 @@ class RolesTest extends TestCase
             ->method('getBySubjectId')
             ->with(2)
             ->willReturn([$membership]);
-        $this->assertEquals([$membership], $this->roles->getRolesForSubject(2));
+        $this->assertEquals([$membership->getRole()], $this->roles->getRolesForSubject(2));
     }
 }
