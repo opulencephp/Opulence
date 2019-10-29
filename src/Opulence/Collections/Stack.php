dd7ed1b7b121f930e8da7fd997443784c39f19e5
Peter Aba <peter@peteraba.com>
2019-10-29 21:27:20 +0100

    Fixes for some of the PhanTypeArraySuspicious* warnings

--- src/Opulence/Collections/Stack.php
+++ src/Opulence/Collections/Stack.php
@@ -20,7 +20,7 @@ use Traversable;
  */
 class Stack implements Countable, IteratorAggregate
 {
-    /** @var The values of the stack */
+    /** @var array The values of the stack */
     protected $values = [];
 
     /**
